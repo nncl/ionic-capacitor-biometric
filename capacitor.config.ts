65cf180d3f4843463472d8d485e8d406c60f78ed
Cauê Almeida <nncl@live.com>
2022-11-04 10:19:42 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'biometric',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
